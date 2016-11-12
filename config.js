30b98937de618d8018d109b9ab64bd2c70c98317
kazuki-nisshin <nisshin@dtdsh.com>
2016-11-13 02:53:17 +0900

    First commit.

---
+++ config.js
@@ -0,0 +1,93 @@
+'use strict';
+/**
+ * タスク設定ファイル
+ */
+module.exports = {
+	IS_PRODUCTION: false,
+	dist: 'public',
+	autoTest: false,
+	ejs: {
+		minify: {
+			collapseWhitespace: true,
+			conservativeCollapse: true,
+			decodeEntities: true,
+			html5: true,
+			minifyCSS: true,
+			minifyJS: true,
+			minifyURLs: false,
+			preserveLineBreaks: true,
+			preventAttributesEscaping: false,
+			quoteCharacter: "",
+			removeEmptyAttributes: true,
+			removeRedundantAttributes: true,
+			removeScriptTypeAttributes: true,
+			removeStyleLinkTypeAttributes: true
+		}
+	},
+	style: {
+		sass: {
+			errLogToConsole: true,
+			outputStyle: 'compressed',
+			sourcemap: true,
+			souceComments: 'normal',
+			includePaths: [
+				'sass',
+				'node_modules/foundation-sites/scss'
+			]
+		},
+		autoprefixer: {
+			browsers: [
+				'last 3 version',
+				'ie 10',
+				'Android 4.2'
+			]
+		},
+		soucemaps: './maps'
+	},
+	js: {
+		sourcemaps: './maps'
+	},
+	browserify: {
+		bundleOption: {
+			cache: {},
+			packageCache: {},
+			fullPaths: false,
+			debug: true,
+			entries: './js/src/hanamaru-app.js',
+			extensions: 'js'
+		},
+		output: './js',
+		filename: 'apps.js'
+	},
+	server: {
+		baseDir: './customer',
+		index: 'index.html'
+	},
+	path: {
+		ejs: {
+			src: ['ejs/**/*.ejs', '!ejs/**/_*.ejs'],
+			watch: 'ejs/**/*',
+			dest: './customer'
+		},
+		style: {
+			src: ['sass/**/*.scss', '!sass/**/_*.scss'],
+			watch: 'sass/**/*.scss',
+			dest: 'customer/assets/css'
+		},
+		svg: {
+			src: 'svg/**/*.svg',
+			watch: 'svg/**/*.svg',
+			dest: 'customer/assets/svg',
+		},
+		js: {
+			src: ['js/*.js', '!js/src/**/*'],
+			watch: 'js/**/*.js',
+			dest: 'customer/assets/js'
+		},
+		image: {
+			src: 'img/**/*',
+			watch: 'img/**/*',
+			dest: 'customer/assets/img/'
+		}
+	}
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,146 +1,44 @@
-var gulp        = require('gulp'),
-	$           = require('gulp-load-plugins')(),
-	browserSync = require('browser-sync');
-var dir = {
-	home: 'www/wordpress/lp/',
-}
-var path = {
-	css: dir.home + 'assets/css/',
-	sass: dir.home + 'assets/sass/',
-	js: dir.home + 'assets/js/',
-	src: dir.home + 'assets/js/src/',
-	precom: dir.home + 'assets/js/pre-compress/',
-	img: dir.home + 'assets/img/',
-	svg: dir.home + 'assets/svg/',
-	svgIcons: dir.home + 'assets/svg/icons/',
-}
-/* =========================================  タスク  ================================ */
+'use strict';
 
-// EJS
-gulp.task( 'ejs', function() {
-	gulp.src( [ dir.home + '/**/*.ejs', '!' + dir.home + '/**/_*.ejs' ] )
-		.pipe( $.ejs() )
-		.pipe( gulp.dest( 'dir.home' ) )
-})
-// COMPASS
-gulp.task('style', function() {
-	gulp.src( [ path.sass + '/**/*.scss', '!' + path.sass + '_*.scss'] )
-		.pipe( $.plumber({
-			errorHandler: function(error) {
-				console.log(error.messageFormatted);
-				this.emit('end');
-			}
-		}))
-		.pipe($.compass({
-			config_file: './config.rb',
-			css: path.css,
-			sass: path.sass,
-			image: path.img,
-			javascript: path.js,
-			comments: false
-		}))
-		.pipe($.filter('**/*.css'))
-		.pipe($.autoprefixer('last 3 version'))
-		.pipe($.cssmin())
-		.pipe(gulp.dest( path.css ))
-		.pipe(browserSync.reload({ stream: true }));
+require('es6-promise').polyfill();
+
+let gulp = require('gulp'),
+	runSequence = require('run-sequence');
+
+// タスクの読み込み
+let tasks = require('./gulp/load'),
+	config = require('./config');
+
+/**
+ * 監視タスク
+ */
+gulp.task('watch', () => {
+	gulp.watch(config.path.ejs.watch, ['html']);
+	gulp.watch(config.path.style.watch, ['style']);
+	gulp.watch(config.path.js.watch, ['preJs']);
+	gulp.watch(config.path.svg.watch, ['svg', 'svg2png']);
+	gulp.watch(config.path.image.watch, ['image']);
 });
 
-// SVG Icons & Images
-// SVG
-gulp.task('svg', function() {
-	gulp.src( path.svgIcons + '/**/*.svg')
-		.pipe( $.plumber({
-			errorHandler: function(error) {
-				console.log(error.messageFormatted);
-				this.emit('end');
-			}
-		}))
-		.pipe($.svgmin())
-		.pipe($.svgstore({ inlineSvg: true }))
-		.pipe($.cheerio({
-			run: function ($, file) {
-				$('svg').addClass('di');
-				$('[fill]').removeAttr('fill');
-			},
-			parserOptions: { xmlMode: true }
-		}))
-		.pipe(gulp.dest(path.svg))
-		.pipe(browserSync.reload({ stream: true }));
-});
-gulp.task('svg2png', function() {
-	gulp.src(path.svgIcons + '/**/*.svg')
-		.pipe( $.plumber({
-			errorHandler: function(error) {
-				console.log(error.messageFormatted);
-				this.emit('end');
-			}
-		}))
-		.pipe($.svg2png(3))
-		.pipe($.rename({
-			prefix: 'di.svg.'
-		}))
-		.pipe(gulp.dest(path.svg))
-		.pipe(browserSync.reload({ stream: true }));
+/**
+ * build タスク
+ */
+gulp.task('build', ['clean'], (callback) => {
+	return runSequence(['html', 'style', 'preJs', 'svg', 'svg2png', 'image', 'browserify', 'copy'], callback);
 });
 
-
-
-// JAVASCRIPT
-// For inline scripts
-gulp.task('precom-scripts', function() {
-	gulp.src( path.precom + '*.js')
-		.pipe( $.plumber({
-			errorHandler: function(error) {
-				console.log(error.messageFormatted);
-				this.emit('end');
-			}
-		}))
-		.pipe($.uglify())
-		.pipe($.rename({
-			extname: '.min.js'
-		}))
-		.pipe(gulp.dest( path.js ))
-		.pipe(browserSync.reload({ stream: true }));
-});
-// Main theme App Javascript
-gulp.task('src-js', function() {
-	gulp.src( path.src + '*.js')
-		.pipe($.concat('hanamaru-app.js'))
-		.pipe( $.plumber({
-			errorHandler: function(error) {
-				console.log(error.messageFormatted);
-				this.emit('end');
-			}
-		}))
-		.pipe($.uglify())
-		.pipe($.rename({
-			extname: '.min.js'
-		}))
-		.pipe(gulp.dest( path.js ))
-		.pipe(browserSync.reload({ stream: true }));
+/**
+ * production タスク
+ */
+gulp.task('production', (callback) => {
+	config.IS_PRODUCTION = true;
+	return runSequence('build', callback);
 });
 
-
-// Browser Sync
-gulp.task('server', function() {
-	browserSync({
-		proxy: 'hanamaru-souzoku.dev/'
-	});
+/**
+ * default タスク
+ */
+var defaultTasks = ['server', 'watch', 'watchify'];
+gulp.task('default', () => {
+	return runSequence(defaultTasks);
 });
-gulp.task('bs-reload', function() {
-	browserSync.reload();
-})
-
-
-
-/* =========================================  WATCH  ================================ */
-gulp.task('watch', [ 'ejs', 'style', 'precom-scripts', 'src-js', 'svg', 'svg2png', 'server' ], function(event) {
-	gulp.watch( dir.home + '{,/**/}{,/**/}{,/**/}{,/**/}{,/**/}*.ejs', ['ejs'] );
-	gulp.watch( path.sass + '{,/**/}{,/**/}{,/**/}{,/**/}{,/**/}*.scss', ['style'] );
-	gulp.watch( path.precom + '{,/**/}{,/**/}{,/**/}{,/**/}{,/**/}*.js', ['precom-scripts'] );
-	gulp.watch( path.src + '{,/**/}{,/**/}{,/**/}{,/**/}{,/**/}*.js', ['src-js'] );
-	gulp.watch( path.svgIcons + '{,/**/}{,/**/}{,/**/}{,/**/}{,/**/}*.svg', ['svg', 'svg2png'] );
-	gulp.watch( dir.home + '{,/**/}{,/**/}{,/**/}{,/**/}{,/**/}*.html', ['bs-reload'] );
-});
-gulp.task('default', ['watch']);
